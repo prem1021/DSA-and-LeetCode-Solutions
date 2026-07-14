fd7eee6e7479006e50fad0260b059966fe616d80
prem1021 <premkumardavkn@gmail.com>
2026-07-14 22:31:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (59.45%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,9 +4,7 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         for (int i = 0; i < intervals.size(); i++) {
-            if (ans.empty() ||
-                intervals[i][0] > ans.back()[1]) { // ya to range me na ho ya to
-                                                   // first interval ho
+            if (ans.empty() || intervals[i][0] > ans.back()[1]) { // ya to range me na ho ya to first interval ho
                 ans.push_back(intervals[i]);
             } else {
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
